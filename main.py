831826b063d8024818524443d73fc02457517549
ekayizzi <123040850+ekayizzi@users.noreply.github.com>
2023-07-21 10:57:51 -0500

    A couple more edits (sparse and directory split)

--- main.py
+++ main.py
@@ -3,24 +3,31 @@
 # Access the bts2 power column and store in data structure
 # look at potential loss functions
 # After training and evaluation, look how to refine the model in your notes ~ using Tensor Board
-
-
-
 # prepare to receive npz files
 
+
+
+# Sparse table library that tensor flow can read in
+
+# Density of tensors
+# You can also construct sparse tensors from dense tensors by using 'tf.sparse.from_dense',
+# and convert them back to dense tensors by using 'tf.sparse.to_dense'
 
 import tensorflow as tf
 import pandas as pd
 import numpy as np
 import datetime
 from sklearn.model_selection import train_test_split
+import splitfolders
 
-# Name of model so we know for training/comparing models
+# Name of model so that we know for training/comparing models
+# k = kernal size    m = max pooling size     f = number of functions
 NAME = "3DConvetTest1-k7m2f128--" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
-
-
-
+# Code for loading in npz files
+# z = np.load(npzPath)
+# print(z.files)   print(z['arr_0'])
+# From there they are loaded in the variable and able to be used
 
 X = ... # variable that will contain 3d voxel data which is contained in numpy arrays
 
@@ -31,12 +38,18 @@ data = pd.read_csv('r'+path)
 signalPower = np.array(data['power of the reciver'])
 
 
-# Variable containing a numpy array of signal power data
+# Variable containing a numpy array of signal power data # 1646 - 3291
 y = signalPower
 
 # Code used to split data, 80% into train data and 20% to testing data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
+# Code that allows for data to be split from the directory into 3 output files
+# How can I do this so that I only run this code?
+# (make another python file in the same project? put the NN into a jupyter notebook? add a def function that outputs
+# files
+# once i run the code below it splits the data up, should I do that in another project? how do you have multiple open at the same time
+directoryPath = '' # name of the file
+splitfolders.ratio(directoryPath, output="output", seed=1337, ratio=(.8, 0.1,0.1))
 
 
 
@@ -79,7 +92,6 @@ model = tf.keras.models.Sequential([
 ])
 
 # Compiles the model with an optimizer function and a loss function
-# Look at what metric you should use
 model.compile(optimizer=tf.train.AdamOptimizer(), loss='mean_squared_error', metrics=['mean_absolute_error'])
 
 # Specify the log directory for TensorBoard
@@ -87,6 +99,7 @@ log_dir = 'logs/{}'.format(NAME)
 
 # Create the TensorBoard callback
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+# To see active results go into terminal and run the /logs command
 
 
 
